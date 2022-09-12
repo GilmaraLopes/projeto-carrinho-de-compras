7c2384ab58912244c9adf1742f8371562a595930
Gilmara <gilmaralopes2006@gmail.com>
2022-09-12 16:20:38 -0300

    requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,8 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const fetchApi = await fetch(url);
+  const products = await fetchApi.json();
+  return products;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,21 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
+const { expect } = require('@jest/globals');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('1.1 Verifique se fetchProducts é uma função.', () => {
+    expect(typeof fetchProducts);
+  });
+  
+  it('1.2 Execute a função fetchProducts com o argumento computador e teste se fetch foi chamada;', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledTimes(1);
+  });
+
+  it('1.3 Verifique se a função fetchProducts foi chamada com o endpoint correto', async () => {
+    await fetchProducts('computador');
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+      expect(fetch).toBeCalledWith(url);
+  });
 });
